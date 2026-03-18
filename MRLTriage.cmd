b1aad584a7a3835b9d3c8ffc75f0a4545c8e9d53
Daniel Feies <1240751+dfeies@users.noreply.github.com>
2026-03-18 10:05:12 -0400

    Update MRLTriage.cmd to improve user prompts and add MixedRealityLink autologger flushing

--- MRLTriage.cmd
+++ MRLTriage.cmd
@@ -4,14 +4,19 @@ cd /d %~dp0
 
 net session >nul 2>&1
 if not %ERRORLEVEL%==0 (
-	echo This script requires administrative privileges. Please run as Administrator.
+	echo This script requires administrative privileges. Right click on file / Run as administrator.
+	set /P DUMMY=Press Enter to close this window...
 	exit /b
 )
 
+echo Starting nvsmi.exe to log GPU events ...
+nvsmi.exe
+
+echo Starting WPR trace (max 1 min), press Enter to stop ...
 set NAME=MRLTriage
 wpr -cancel >nul 2>&1
 wpr -start %NAME%.wprp -filemode
-set /P DUMMY=Press Enter to stop tracing...
+timeout /t 60
 
 set hour=%time:~0,2%
 if "%hour:~0,1%" == " "  set hour=0%hour:~1,1%
@@ -21,8 +26,22 @@ set ETL=%TimeStamp%_%NAME%.etl
 wpr -stop %ETL% -compress -skipPdbGen 
 echo %ETL%
 
-:: copy unity logs
-copy /Y "%LOCALAPPDATA%\Unity\Editor\Editor.log" "%TimeStamp%_%NAME%_Editor.log"
+echo Flushing the MixedRealityLink autologger ...
+logman.exe update MixedRealityLinkLog -fd -ets
+if %ERRORLEVEL% neq 0 (
+	echo ERROR: Failed to flush MixedRealityLinkLog. ERRORLEVEL=%ERRORLEVEL%
+	exit /b %ERRORLEVEL%
+)
+
+echo Copying MixedRealityLink ETLs to this folder ...
+copy /Y %SystemRoot%\system32\LogFiles\MixedRealityLink.etl* %~dp0
+if %ERRORLEVEL% neq 0 (
+	echo ERROR: Failed to copy MixedRealityLink ETL files. ERRORLEVEL=%ERRORLEVEL%
+	exit /b %ERRORLEVEL%
+)
+
+for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
+echo %ESC%[92mDone. Send the ETLs from %~dp0 for analysis (*.etl and *.etl.00n).%ESC%[0m
 
 set /P DUMMY=Press Enter to close this window...
 endlocal
